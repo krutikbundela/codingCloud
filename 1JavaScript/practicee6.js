94dbddc865367ef06ab24800a159b76c2782486b
itzarbxx <arbaazshaikh9397@gmail.com>
2024-04-02 21:33:47 -0700

    array practice

---
+++ 1JavaScript/practicee6.js
@@ -0,0 +1,34 @@
+// let fruits = ["apple", "arbaaz", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple", "orange", "arbaaz",];
+
+// console.log(fruits);
+// console.log(fruits.length);
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[86]);
+// console.log(fruits[24]);
+// console.log(fruits[88]);
+
+// let firstcolor = `blue`;
+// let secondcolor1= `yellow`;
+
+// let secondcolor = `Hello, How Are You? ${firstcolor} ${40+50} ${secondcolor}`;
+// // console.log(secondcolor);
+
+
+// let multiLineString = `
+// hello how are you
+// hello
+// hi
+//     2
+//     3
+//     4
+//     5
+//     5
+// `;
+
+// console.log(multiLineString);
+
+// let num = 5;
+// console.log("5 * " + num + " = " + 5 * num);
+// console.log(`5 * ${num} = ${5 * num}`);
+
